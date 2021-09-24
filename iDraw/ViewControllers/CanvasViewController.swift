b6b37ca1d9e6a29d3521d376703c458e6b64b57f
Marat Nazirov <maratnazirov87@gmail.com>
2021-09-24 17:12:20 +0300

    Adding drawing capability

--- iDraw/ViewControllers/CanvasViewController.swift
+++ iDraw/ViewControllers/CanvasViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class CanvasViewController: UIViewController {
+    
+    private let canvas = Canvas()
 
     override func loadView() {
         view = UIView()
         view.backgroundColor = .white
     }
-
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setCanvas()
+    }
+    
+    private func setCanvas() {
+        view.addSubview(canvas)
+        canvas.backgroundColor = .white
+    }
+    
+    override func viewWillLayoutSubviews() {
+        canvas.frame = view.frame
+    }
 }
 
--- iDraw/Views/Canvas.swift
+++ iDraw/Views/Canvas.swift
@@ -8,13 +8,30 @@
 import UIKit
 
 class Canvas: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
+    
+    private var lines: [CGPoint] = []
+    
     override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+        super.draw(rect)
 
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        
+        for (i, p) in lines.enumerated() {
+            if i == 0 {
+                context.move(to: p)
+            } else {
+                context.addLine(to: p)
+            }
+        }
+        
+        context.strokePath()
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+
+        guard let point = touches.first?.location(in: nil) else { return }
+        lines.append(point)
+        
+        setNeedsDisplay()
+    }
 }
