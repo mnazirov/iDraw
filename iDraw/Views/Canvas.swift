16d0fadd549d90a0f772f44b7f07ce886aea1c3f
Marat Nazirov <maratnazirov87@gmail.com>
2021-09-25 12:11:02 +0300

    Fixing the continuous line bug

--- iDraw/Views/Canvas.swift
+++ iDraw/Views/Canvas.swift
@@ -9,28 +9,39 @@ import UIKit
 
 class Canvas: UIView {
     
-    private var lines: [CGPoint] = []
+    private var lines: [[CGPoint]] = [[]]
     
     override func draw(_ rect: CGRect) {
         super.draw(rect)
 
         guard let context = UIGraphicsGetCurrentContext() else { return }
         
-        for (i, p) in lines.enumerated() {
-            if i == 0 {
-                context.move(to: p)
-            } else {
-                context.addLine(to: p)
+        lines.forEach { line in
+            for (i, p) in line.enumerated() {
+                if i == 0 {
+                    context.move(to: p)
+                } else {
+                    context.addLine(to: p)
+                }
             }
         }
         
         context.strokePath()
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lines.append([CGPoint]())
+        print(lines)
+    }
+    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
 
         guard let point = touches.first?.location(in: nil) else { return }
-        lines.append(point)
+        
+        guard var lastElement = lines.popLast() else { return }
+        lastElement.append(point)
+        
+        lines.append(lastElement)
         
         setNeedsDisplay()
     }
